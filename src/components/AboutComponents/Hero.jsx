70103c6cf4d7e4603caf1e1810a537876294166d
lakshay <lakshay8219@gmail.com>
2025-04-02 10:29:03 +0530

    dynamic hero

--- src/components/AboutComponents/Hero.jsx
+++ src/components/AboutComponents/Hero.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Hero = () => {
+const Hero = ({data}) => {
   return (
     <div className="relative w-full">
       
-      <img src="/images/aboutimg/hero.png" alt="Hero Image" className="w-full h-auto object-cover" />
+      <img src={data.img_Src} alt="Hero Image" className="w-full h-auto object-cover" />
 
      
       <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl sm:text-4xl md:text-5xl font-bold">
-        About Us
+        {data.heading}
       </h1>
     </div>
   );
