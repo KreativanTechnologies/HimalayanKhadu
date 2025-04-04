7af92366ea09f7d2f40423b9f78c4e6beea00954
jatinrajwal15 <rajwaljatin15@gmail.com>
2025-04-04 10:17:00 +0530

    Login

---
+++ src/components/Logincomp/Form.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+
+const Form = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted Data:", formData);
+  };
+
+  return (
+    <div
+      className="relative w-full h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/Loginimg/Rectangle 1 (1).png')" }}
+    >
+      {/* Container */}
+      <div className="bg-white/30 backdrop-blur-xl shadow-lg rounded-3xl p-8 w-96 border border-white/30">
+        <h2 className="text-2xl font-bold text-center text-[#0C8699]  mb-4">
+          Sign in
+        </h2>
+
+        <form onSubmit={handleSubmit}>
+          {/* Username Input */}
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+            className="w-full px-4 py-2 mb-3 rounded-full bg-white/10 text-teal-900 placeholder-gray-600 focus:ring-2 focus:ring-teal-400 focus:outline-none backdrop-blur-2xl"
+          />
+          
+          {/* Password Input */}
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full px-4 py-2 mb-3 rounded-full bg-white/10 text-teal-900 placeholder-gray-600 focus:ring-2 focus:ring-teal-400 focus:outline-none backdrop-blur-2xl"
+          />
+
+          {/* Forgot Password */}
+          <div className="text-sm text-[#0C8699] font-semibold hover:underline cursor-pointer text-left mb-3">
+            Forgot Password?
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full bg-teal-600 text-white py-2 rounded-full hover:bg-teal-700 transition shadow-md"
+          >
+            Submit
+          </button>
+        </form>
+
+        {/* Signup Link */}
+        <p className="text-center text-sm text-[#0C8699] font-semibold  mt-4">
+          Don’t have an Account?{" "}
+          <span className="text-[#0C8699]  font-bold hover:underline cursor-pointer">
+            Sign up
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
