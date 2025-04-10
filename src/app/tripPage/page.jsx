d9ff0f3b272a85ac1e54538e571c1e68b094016f
lakshay <lakshay8219@gmail.com>
2025-04-10 20:23:35 +0530

    update

--- src/app/tripPage/page.jsx
+++ src/app/tripPage/page.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import Hero from '../../components/tripPageComponents/Hero'
-import Section2 from '../../components/tripPageComponents/section2'
-import Section3 from '../../components/tripPageComponents/Section3'
-import DestinationSlider from '../../components/DestinationsComp/DestinationSlider'
-import DiscoverTrips from '../../components/AboutComponents/OurPackages'
-import ClientSay from '../../components/HomePageComp/ClientSay'
+import Hero from '../../components/AboutComponents/Hero';
+import ItineraryPage from '../../components/TrippageComp/ItineraryPage';
+import BatchPricingInfo from '../../components/TrippageComp/BatchPricingInfo';
+import DestinationSlider from '../../components/DestinationsComp/DestinationSlider';
+import ClientSay from "../../components/HomePageComp/ClientSay";
+import RelatedTrip from '../../components/TrippageComp/RelatedTrip';
+const Herodata = {
+    img_Src : "/images/Cyclingimg/Rectangle 1.png",
+  }
 
 const page = () => {
   return (
-    <>
-      
-    <Hero/>
-    <Section2/>
-    <Section3/>
-    < DestinationSlider />
-    < ClientSay />
-    < DiscoverTrips  />
-    </>
+    <div>
+      <Hero data={Herodata}/>
+      <ItineraryPage/>
+      <BatchPricingInfo/>
+      <DestinationSlider/>
+      <ClientSay />
+      <RelatedTrip/>
+    </div>
   )
 }
 
