cb620393ee8de95cc2abf34de8d02b3b1c4ab70b
lakshay <lakshay8219@gmail.com>
2025-04-14 13:48:39 +0530

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-
-
-
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   output: 'export',
-//   trailingSlash: true, // 👈 IMPORTANT
+//   /* config options here */
 // };
 
 // export default nextConfig;
+
+
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true, // 👈 IMPORTANT
+};
+
+export default nextConfig;
