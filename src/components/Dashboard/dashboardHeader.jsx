9e78de1864fccb565291ede8f4984afa53259524
Rahul Yadav <rahulyad8860@gmail.com>
2025-04-17 11:08:24 +0530

    'button'

--- src/components/Dashboard/dashboardHeader.jsx
+++ src/components/Dashboard/dashboardHeader.jsx
@@ -1,13 +1,21 @@
+"use client"
+
 export function DashboardHeader({ heading, text, children }) {
-    return (
-      <div className="flex items-center justify-between px-2">
-        <div className="grid gap-1">
-          <h1 className="font-heading text-3xl  md:text-4xl">{heading}</h1>
-          {text && <p className="text-lg text-muted-foreground">{text}</p>}
-        </div>
-        {children}
+  const handleLogout = () => {
+   
+  };
+  return (
+    <div className="flex items-center justify-between  px-2">
+      <div className="grid gap-1">
+        <h1 className="font-heading text-3xl  md:text-4xl">{heading}</h1>
+        {text && <p className="text-lg text-muted-foreground">{text}</p>}
+
       </div>
-    )
-  }
-  
-  
+      <div className="gap-2 flex items-center justify-end ">
+        {children}
+        <button className="bg-gray-300 rounded-sm px-2 py-1 hover:bg-gray-400 cursor-pointer" onClick={handleLogout}>Log-Out</button>
+      </div>
+    </div>
+  )
+}
+
